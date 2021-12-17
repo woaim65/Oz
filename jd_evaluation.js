ab5af206a1ead4d4e05551ab6223263f778595f0
KingRan <754879133@qq.com>
2021-12-17 14:11:58 +0800

    更新

--- jd_evaluation.js
+++ jd_evaluation.js
@@ -156,7 +156,7 @@ function getOrderList(orderType,startPage,pageSize){
                     console.log(`${$.name} API请求失败，请检查网路重试`);
                 } else {
                     if (safeGet(data)) {
-                        data = $.toObj(data);
+                        data = JSON.parse(data);
                         if (data.errCode === '0') {
                             if (data.orderList && data.orderList.length) {
                                 for (let da of data.orderList) {
@@ -242,8 +242,8 @@ function sendEval(item){
                     console.log(`${$.name} API请求失败，请检查网路重试`);
                 } else {
                     if (safeGet(data)) {
-                        data = $.toObj(data);
-                        if (data.errMsg === 'success') {
+                        data = JSON.parse(data);;
+                        if (data.iRet === 0) {
                             console.log('普通评价成功！');
                         } else {
                             console.log('普通评价失败了.....');
@@ -297,7 +297,7 @@ function sendServiceEval(item){
                     console.log(`${$.name} API请求失败，请检查网路重试`);
                 } else {
                     if (safeGet(data)) {
-                        data = $.toObj(data);
+                        data = JSON.parse(data);;
                         if (data.errMsg === 'success') {
                             console.log('服务评价成功！');
                         } else {
@@ -355,7 +355,7 @@ function appendComment(item){
                     console.log(`${$.name} API请求失败，请检查网路重试`);
                 } else {
                     if (safeGet(data)) {
-                        data = $.toObj(data);
+                        data = JSON.parse(data);;
                         if (data.errMsg === 'success') {
                             console.log('晒单成功！');
                         } else {
@@ -536,6 +536,9 @@ function TotalBean() {
 }
 function safeGet(data) {
     try {
+        if(data.indexOf('json(') === 0){
+            data = data.replace(/\n/g, "").match(new RegExp(/json.?\((.*);*\)/))[1]
+        }
         if (typeof JSON.parse(data) == "object") {
             return true;
         }
